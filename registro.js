6e70a992f0da0fd659dc85f637dc974a9d3f1a38
lazarod07 <lazarod@uninorte.edu.co>
2020-04-22 16:10:09 -0500

    upload image fixed

--- registro.js
+++ registro.js
@@ -7,21 +7,44 @@ firebase.auth().onAuthStateChanged(function(user) {
     var idDoc = document.getElementById("IDdoc").value;
     var company = document.getElementById("company").value;
     var imag = document.getElementById("inputGroupFile01").files[0];
+    var imge = "";
      
     
     if (user) {
-        uid = firebase.auth().currentUser.uid;
-        console.log(imag);
-        var imge=uploadimg(imag);
-        console.log(imge);
 
-        firebase.database().ref("users/" + uid).set({
-            "email": useremail,
-            "password": userpassword,
-            "uid": uid,
-        }).then(function(){
-            // location.href = "operators.html";
+        uid = firebase.auth().currentUser.uid;
+
+        var storageRef = firebase.storage().ref();
+
+         // Create a reference to 'mountains.jpg'
+        var mountainsRef = storageRef.child('images/'+imag.name).put(imag);
+
+
+        mountainsRef.on('state_changed', function(snapshot){
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        
+        }, function(error) {
+            // Handle unsuccessful uploads
+        }, function() {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            mountainsRef.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log(downloadURL);
+                firebase.database().ref("users/" + uid).set({
+                    "email": useremail,
+                    "password": userpassword,
+                    "uid": uid,
+                }).then(function(){
+                    
+                    // location.href = "operators.html";
+                });
+
+            }); 
         });
+
+
+        
 
         
         
@@ -84,15 +107,33 @@ function uploadimg(imag){
     // Create a reference to 'mountains.jpg'
     var mountainsRef = storageRef.child('images/'+imag.name).put(imag);
 
-    mountainsRef.on('state_changed',
-    function(snapshot){
+    // mountainsRef.on('state_changed',
+    // function(snapshot){
 
-    }, function(error){
-        window.alert(error.message);
-    },function(){
-        var URLimag = mountainsRef.snapshot.downloadURL;
-        return URLimag;
-    });
+    // }, function(error){
+    //     window.alert(error.message);
+    // },function(){
+    //     var URLimag = mountainsRef.snapshot.downloadURL;
+    //     return URLimag;
+    // });
+
+
+    mountainsRef.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        mountainsRef.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          console.log('File available at', downloadURL);
+          imge = downloadURL;
+          console.log('cccccccccccc', imge);
+        });
+      });
+      
 
     // Create a reference to 'images/mountains.jpg'
    // var mountainImagesRef = storageRef.child('images/mountains.jpg');
