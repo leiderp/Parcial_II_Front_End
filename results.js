3b3e54e0fcdf133b703ec829c6fb2fadff7e0183
lazarod07 <lazarod@uninorte.edu.co>
2020-04-25 03:24:16 -0500

    final

--- results.js
+++ results.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function responder(){
     var tbody = document.getElementById("scrol");
     console.log("reaccionon");
@@ -55,35 +54,6 @@ function responder(){
                         }).then(function(){
                                     
                         });
-=======
-uuid = localStorage.uid;
-var us = db.ref("users/" + uuid);
-var query = db.ref("questions");
-
-us.on("value", function(snapshot2) {
-    query.orderByChild("uid").equalTo(snapshot.val().uidcompany).on("value", function(snapshot) {
-        snapshot.forEach(function(data) {
-            var pregunta = "";
-            var check = false;
-            var opcioncorrect = "";
-            var total = 0;
-            var tocalCorrects = 0;
-            for (var i = 1; i < 6; i++) {
-                pregunta = document.getElementById("pregunta"+i).textContent;
-                check = document.getElementById("radio"+i+"1").checked;
-                //var valorAns = document.getElementById("score"+i).value;
-                if(check==true){
-                    opcioncorrect=document.getElementById("radio"+i+"1").value;
-                }else{
-                    opcioncorrect=document.getElementById("radio"+i+"2").value;
-                }
-
-                if(data.val().question == pregunta){
-                    total=total+data.val().value;
-                    if(data.val().answer == opcioncorrect){
-                        varlQ=data.val().value;
-                        vale = vale+valQ;
->>>>>>> 66ecb1086052d9ea17db6718ab8123e6cff0a804
                     }
                 }
             });
@@ -94,7 +64,6 @@ us.on("value", function(snapshot2) {
     });
 
 
-<<<<<<< HEAD
 }
 
 function generateHexString(length) {
@@ -104,7 +73,3 @@ function generateHexString(length) {
     }
     return ret.substring(0,length);
 }
-=======
-});
-
->>>>>>> 66ecb1086052d9ea17db6718ab8123e6cff0a804
